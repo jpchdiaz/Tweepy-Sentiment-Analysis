b5fcee318ef472d67aaa35d1208b4537a8334927
jpitched <jpchdiaz@gmail.com>
2018-06-27 08:11:30 -0400

    test upload 2 and a bit of progress

---
+++ Projects/News Mood/config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "vn3mmoiC4rPJ3dU81RGKkafpX"
+consumer_secret = "kpQmo2TVBsaHf1byjtj0s73y2bkBzthZFPDutrS0a38lXeIgZ3"
+access_token = "1002556576534757376-Hx30EWvA72gsZJ6nk2fAwcq6h34BxD"
+access_token_secret = "jxRQXOxJ2q7IWphiVwBT5VHZWFR8OwHaJmfurzVjxf7Kd"
