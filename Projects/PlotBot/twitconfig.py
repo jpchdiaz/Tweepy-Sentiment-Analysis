aea5daa28f857dc91daa81ba30f3eb4d8c5eddae
jpitched <jpchdiaz@gmail.com>
2018-06-28 16:58:32 -0400

    Added Analysis for News Mood

---
+++ Projects/PlotBot/twitconfig.py
@@ -0,0 +1,6 @@
+# Twitter API Keys
+bot_url = 'https://twitter.com/lumpytweetbot'
+consumer_key = "pBJ0YjtpU90vDGGWZbutpJKw7"
+consumer_secret = "IMgv4HydMFwhYee8ZZUWbaPMimPUDTGH116IKCUcxE6uLDvQtS"
+access_token = "1012421138201432064-Qn9mJAPyiZ4aENHM6QOQ0I05MjuRbH"
+access_token_secret = "en5deM8cHXk19jDVuskpvMuneTi1pmSo4gyTjEjmCj6wc"
